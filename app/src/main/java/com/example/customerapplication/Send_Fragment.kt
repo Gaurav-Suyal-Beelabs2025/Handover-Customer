a0258388ac5538691f9a2ba9ba18c9fe11525369
amitsaxena383828 <amitsaxena383828@gmail.com>
2023-06-13 11:45:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/customerapplication/Send_Fragment.kt
@@ -0,0 +1,18 @@
+package com.example.customerapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+ class Send_Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_send_, container, false)
+    }
+
+}
